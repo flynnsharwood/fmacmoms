7e296820bdd3cb19f589f3a3d2ccf458c6af84a7
unknown <harshil@harshildari.net>
2025-07-01 17:09:34 +0530

    too lazy to add message

--- src/pages/index.js
+++ src/pages/index.js
@@ -40,8 +40,8 @@ export default function Home() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      title={`${siteConfig.title}`}
+      description="Moments of meets">
       <HomepageHeader />
       <main>
         {/* <HomepageFeatures /> */}
